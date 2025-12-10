4233a487e3a50be0d66b94d889fde7e74bd7ac56
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-10 17:34:16 +0100

    another method

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const button= document.getElementById("btn");
-const lightElement= document.getElementById("light");
+/*const button= document.getElementById("btn");
+const lightElement= document.getElementById("lamp-on");
 const wrapper= document.querySelector(".wrapper")
 //console.log(wrapper)
 //console.log(button)
 
-
+/*
 //ho semplicemente cambiato la source dell'immagine lightElement
 button.addEventListener("click", ()=>{ 
     lightElement.src="img/yellow_lamp.png"
@@ -12,4 +12,17 @@ button.addEventListener("click", ()=>{
     button.innerText="Spegni"
     //ho dato un div esterno a cui dare un background
     wrapper.style.backgroundColor="white"
-})
+})*/
+
+
+const button= document.getElementById("btn");
+const lightOn= document.querySelector(".lamp-on");
+const lightOff= document.querySelector(".lamp-off")
+const wrapper= document.querySelector(".wrapper-second-try")
+
+button.addEventListener("click", ()=>{ 
+    lightOn.style.display="block";
+    lightOff.style.display="none"
+    button.innerText="Spegni"
+    wrapper.style.backgroundColor="white"
+console.log("cliccato")})
