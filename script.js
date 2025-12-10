1414bd2ed622888fcb7777859aaa514a5238c2c0
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-10 16:21:35 +0100

    add background on click event

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const button= document.getElementById("btn");
 const lightElement= document.getElementById("light");
+const wrapper= document.querySelector(".wrapper")
+console.log(wrapper)
 //console.log(button)
 
 
 //ho semplicemente cambiato la source dell'immagine lightElement
 button.addEventListener("click", ()=>{ 
     lightElement.src="img/yellow_lamp.png"
+    //cambio innerText di btn
     button.innerText="Spegni"
+    //ho dato un div esterno a cui dare un background
+    wrapper.style.backgroundColor="black"
 })
